009bc77594d448c443c121de9cf4b29cd46e7653
LaxmiSathy <laxmi.sathy@gmail.com>
2018-02-06 13:57:24 +0530

    api key changed

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -28,7 +28,7 @@ app.get('/api/:source/:destination', function(req,res){
         params: {
           origin: source,
           destination: destination,
-          key: 'AIzaSyBGkDumezTWqlCXMjnog68fqh_-TrSxmVw'
+          key: 'AIzaSyCLdlqDqHnZBXq45hyrJ2o5ptRlxU3BhD8'
         }
       })
   .then(function(response){
