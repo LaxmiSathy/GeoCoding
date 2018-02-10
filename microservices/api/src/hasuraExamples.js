7a5569fbfcc16b120902573924287fb59bdb5893
LaxmiSathy <laxmi.sathy@gmail.com>
2018-02-10 21:19:23 +0530

    select query for table sp_user

--- microservices/api/src/hasuraExamples.js
+++ microservices/api/src/hasuraExamples.js
@@ -9,7 +9,7 @@ router.route("/").get(function (req, res) {
 
 router.route("/get_articles").get(function (req, res) {
   console.log("Get articles");
-  //Fetch all rows from table - articles
+  //Fetch all rows from table - article
   var selectOptions = {
     url: config.projectConfig.url.data,
     method: 'POST',
@@ -60,7 +60,15 @@ router.route("/show_users").get(function (req, res) {
       'args': {
         'table': 'sp_user',
         'columns': [
-          '*'
+          'user_id',
+          'username',
+          'amount'
+        ],
+        'order_by': [
+            {
+                'column': 'user_id',
+                'order': 'asc'
+            }
         ]
       }
     })
@@ -77,6 +85,50 @@ router.route("/show_users").get(function (req, res) {
     res.json(JSON.parse(body))
   })
 })
+
+//select users based on user id
+router.route("/user_only").get(function (req, res) {
+  console.log("Show User list");
+  //Fetch a row from table - sp_user
+  var uid = req.param('id');
+  var selectOptions = {
+    url: config.projectConfig.url.data,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Hasura-User-Id': 3,
+      'X-Hasura-Role': 'user'
+    },
+    body: JSON.stringify({
+      'type': 'select',
+      'args': {
+        'table': 'sp_user',
+        'columns': [
+          'user_id',
+          'username',
+          'amount'
+        ],
+        "where": {
+            "user_id": {
+                "$eq": "uid"
+            }
+        }
+      }
+    })
+  }
+  request(selectOptions, function(error, response, body) {
+    if (error) {
+        console.log('Error from select request: ');
+        console.log(error)
+        res.status(500).json({
+          'error': error,
+          'message': 'Select request failed'
+        });
+    }
+    res.json(JSON.parse(body))
+  })
+})
+
 
 
 
