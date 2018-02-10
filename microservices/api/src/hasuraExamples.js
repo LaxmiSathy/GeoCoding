fd258a20bcfc0451418f9b79e48da1b3d8a61cc7
LaxmiSathy <laxmi.sathy@gmail.com>
2018-02-10 15:45:18 +0530

    splitwise user table query

--- microservices/api/src/hasuraExamples.js
+++ microservices/api/src/hasuraExamples.js
@@ -41,4 +41,43 @@ router.route("/get_articles").get(function (req, res) {
   })
 })
 
+//Splitwise Project End points
+
+//Shows the list of users and their amount due
+router.route("/show_users").get(function (req, res) {
+  console.log("Show User list");
+  //Fetch all rows from table - articles
+  var selectOptions = {
+    url: config.projectConfig.url.data,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Hasura-User-Id': 3,
+      'X-Hasura-Role': 'user'
+    },
+    body: JSON.stringify({
+      'type': 'select',
+      'args': {
+        'table': 'sp_user',
+        'columns': [
+          '*'
+        ]
+      }
+    })
+  }
+  request(selectOptions, function(error, response, body) {
+    if (error) {
+        console.log('Error from select request: ');
+        console.log(error)
+        res.status(500).json({
+          'error': error,
+          'message': 'Select request failed'
+        });
+    }
+    res.json(JSON.parse(body))
+  })
+})
+
+
+
 module.exports = router;
