ea1a5a54c571d1f59b2756f2f1f9e4a90b087147
LaxmiSathy <laxmi.sathy@gmail.com>
2018-02-27 18:13:52 +0530

    hasura secret error fix

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({
 
 app.use('/', hasuraExamplesRouter);
 
-app.get('/get_direction', function(req,res){
+app.get('/directions', function(req,res){
   var source = req.query.source;
   var destination = req.query.destination;
   var mode = req.query.mode;
